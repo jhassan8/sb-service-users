864538564184bb13d74717f91d700f6f74f7887d
Jocs <jhassan8@outlook.com>
2019-09-28 18:42:15 -0300

    add expose id rest repository

---
+++ src/main/java/com/app/users/RepositoryConfig.java
@@ -0,0 +1,20 @@
+package com.app.users;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.app.users.models.entity.Role;
+import com.app.users.models.entity.User;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(User.class, Role.class);
+	}
+
+	
+	
+}
