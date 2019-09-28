9d71a10aa88bc57acbcabdea704a39c3123b0f47
Jocs <jhassan8@outlook.com>
2019-09-28 14:12:31 -0300

    custom endpoint in rest repository

--- src/main/java/com/app/users/models/repository/UserRepository.java
+++ src/main/java/com/app/users/models/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package com.app.users.models.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.app.users.models.entity.User;
 
 @RepositoryRestResource(path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long>{
 
-	public User findByUsername(String username);
+	@RestResource(path = "byUsername")
+	public User findByUsername(@Param("username") String username);
 	
 }
