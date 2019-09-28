f84c2f7a2b5998a0195d26540fd31862c8c013e1
Jocs <jhassan8@outlook.com>
2019-09-28 19:48:25 -0300

    add dependency user commmons

--- src/main/java/com/app/users/SpringbootServiceUsersApplication.java
+++ src/main/java/com/app/users/SpringbootServiceUsersApplication.java
@@ -2,8 +2,10 @@ package com.app.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.app.users.commons.models.entity"})
 public class SpringbootServiceUsersApplication {
 
 	public static void main(String[] args) {
