ac030723e9b4beeaa0161859a879b838de233dcf
Jocs <jhassan8@outlook.com>
2019-09-29 02:51:42 -0300

    implementing event three attempts at login

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, password, first_name, last_name, email, enabled) VALUES('admin', '$2a$10$1o4pxQM9xscbgUrFgzgKAedAsIAjNneeU03vQTxJJMBzftvuPbvTC', 'admin', 'admin', 'admin@email.com', 1);
-INSERT INTO users (username, password, first_name, last_name, email, enabled) VALUES('user', '$2a$10$eKO2cw9MknKnPxxF7f3LdO2aJzIm2hWDPRTBoWLxPBwCbxcHBk56u', 'user', 'user', 'user@email.com', 1);
+INSERT INTO users (username, password, first_name, last_name, email, enabled, tries) VALUES('admin', '$2a$10$1o4pxQM9xscbgUrFgzgKAedAsIAjNneeU03vQTxJJMBzftvuPbvTC', 'admin', 'admin', 'admin@email.com', 1, 0);
+INSERT INTO users (username, password, first_name, last_name, email, enabled, tries) VALUES('user', '$2a$10$eKO2cw9MknKnPxxF7f3LdO2aJzIm2hWDPRTBoWLxPBwCbxcHBk56u', 'user', 'user', 'user@email.com', 1, 0);
 
 INSERT INTO roles (name) VALUES ('ROLE_USER');
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
